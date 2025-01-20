eda3d4b42cc22f818fc0141b7118ad2f4c0ad4fb
Sandoval Mario <mdyoc74@gmail.com>
2025-01-20 16:58:10 +0900

    Add result header and corrected some minor graphic interphase's issues, and the arithmetic functions for the page

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log("Working!")
+function add(first, second) {
+    return first + second;
+}
+function subtract(first, second) {
+    return first - second;
+}
+function multiply(first, second) {
+    return first * second;
+}
+function divide(first, second) {
+    if (second == 0) {
+        return 'Error'
+    }else{
+        return first / second;
+    }
+}
+
